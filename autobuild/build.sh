d55e0cfeb1b50d8493f6d1d07b10362889521162
Mario Couture <MarioC@bitheads.com>
2019-03-05 10:46:27 -0500

    Added version in jar file name

--- autobuild/build.sh
+++ autobuild/build.sh
@@ -3,9 +3,23 @@
 # Go to project root
 cd `dirname $0`/..
 
+if [ ! -f version.txt ]; then
+    echo "Missing version.txt file, must contain onle line with the current version of the source."
+    exit 1
+fi
+
+VERSION=`cat version.txt`
+
+#Get Version from arguments if present
+if [ -n "$1" ]; then
+    VERSION=$1
+fi
+
+echo "Building Brainclouds2s... "
 mkdir -p build
 javac -d ./build `find ./src -name "*.java"`
+echo "Packacking Brainclouds2s-$VERSION.jar..."
 mkdir -p dist
 pushd ./build
-jar -cf ../dist/Brainclouds2s.jar `find . -name "*.class"`
-
+jar -cf ../dist/Brainclouds2s-$VERSION.jar `find . -name "*.class"`
+echo "Done."
--- autobuild/test.sh
+++ autobuild/test.sh
@@ -3,10 +3,17 @@
 # Go to project root
 cd `dirname $0`/..
 
+VERSION=`cat version.txt`
+
+# Make sure it is already built
+if [ ! -f dist/Brainclouds2s-$VERSION.jar ]; then
+    autobuild/build.sh
+fi
+
 # Set a temporary build folder
 mkdir -p testBuild
-javac -cp lib/junit-4.12.jar -d ./testBuild `find ./src -name "*.java"` `find ./test -name "*.java"`
-java -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:testBuild/ org.junit.runner.JUnitCore com.bitheads.braincloud.s2s.Brainclouds2sSetupTest
+javac -cp lib/junit-4.12.jar:dist/Brainclouds2s-$VERSION.jar -d ./testBuild `find ./test -name "*.java"`
+java -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar:dist/Brainclouds2s-$VERSION.jar:testBuild/ org.junit.runner.JUnitCore com.bitheads.braincloud.s2s.Brainclouds2sSetupTest
 # Persist test results
 TEST_RESULT=$?
 
